abde87af37bb8d20c0aa4fe8ac56ef7a535d2752
Geon_27 <yukgeon98527@gmail.com>
2022-11-08 15:13:57 +0900

    1108_final

--- Scene-MultiUnwind/ViewController.swift
+++ Scene-MultiUnwind/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func gotoPage1(_sender: UIStoryboardSegue) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
