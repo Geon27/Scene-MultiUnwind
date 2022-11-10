570d957effeb8b7d4c4c54d61a42647a877b4aaf
Geon_27 <yukgeon98527@gmail.com>
2022-11-10 16:54:23 +0900

    1110_final

---
+++ Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 육건 on 2022/11/10.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController{
+    
+    @IBAction func gotoPage2(_segue: UIStoryboardSegue) {
+        
+    }
+}
